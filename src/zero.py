b3cd6f04896f502e17efe104e16e094bf12d9dd9
Rafael Guterres Jeffman <rafasgj@gmail.com>
2024-03-20 19:41:11 -0300

    Rafael Guterres Jeffman
    Solução parcial para o exercício.
    
    Olhe a aba _Checks_ e veja os resultados.

--- src/zero.py
+++ src/zero.py
@@ -10,3 +10,8 @@ def procura_maior(lista):
         if item > maior:
             maior = item
     return maior
+
+
+def procura_menor(lista):
+    """Retorna o menor item da lista."""
+    return min(lista)
