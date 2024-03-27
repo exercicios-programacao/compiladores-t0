e3e263e39f13f6f7de569af1db1a27970bf274ea
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-03-27 16:54:33 -0300

    Nome: Kauã segabinazzi dificuldade: senti dificuldade na questão dos teste pois eu não tenho o habito de programar e testar no python. Então tive que verificar como fazer esses testes na web para testar o codigo.

--- src/zero.py
+++ src/zero.py
@@ -2,7 +2,6 @@
 
 """Implementação do exercício zero."""
 
-
 def procura_maior(lista):
     """Procura maior item na lista usando procura linear."""
     maior = lista[0]
@@ -10,3 +9,22 @@ def procura_maior(lista):
         if item > maior:
             maior = item
     return maior
+
+def procura_menor(lista):
+    # """Procura o menor item na lista."""
+    menor = lista[0]
+    for item in lista[1:]:
+        if item < menor:
+            menor = item
+    
+    return menor
+
+def procura_impares(lista):
+       # """Retorna os numeros impares de uma lista, utilizando a expressao 
+       # lambda que verifica se o numero é impar com os parametros x: x % 2 != 0 """
+    return list(filter(lambda x: x % 2 != 0, lista))
+
+def procura_pares(lista):
+       # """Retorna os numeros impares de uma lista, utilizando a expressao 
+       # lambda que verifica se o numero é pares com os parametros x: x % 2 == 0 """
+    return list(filter(lambda x: x % 2 == 0, lista))
